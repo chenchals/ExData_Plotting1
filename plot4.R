10e50f623113f78c07bdb6165841c136325a2ebf
Chenchal Subraveti <chenchals@gmail.com>
2014-10-12 10:58:06 -0500

    Initital commit - looks good on screen , but not good for bitmap image as fonts are too big. Retrying with rendering directly to PNG device

---
+++ plot4.R
@@ -0,0 +1,51 @@
+## plot4.R
+
+# Read data should be in a separate file, instead of copying the same lines.
+################ Read Data necessary for this plot ############################
+# Data source: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# Download prior to analysis
+# size: wc: 2075260 2075260 132960755 household_power_consumption.txt
+# Columns are ";" separated
+library(data.table) ## for fast read
+# Date: dd/mm/yyyy Time: hh:mm:ss
+# Load only data from the dates 2007-02-01 and 2007-02-02.
+#value from inspecting 1st row
+firstDateTime=strptime("16/12/2006 17:24:00","%d/%m/%Y %H:%M:%S")
+skipTillDateTime=strptime("1/2/2007 00:00:00","%d/%m/%Y %H:%M:%S")
+## 1 row per minute, add 1 for header
+nRowsToSkip=difftime(skipTillDateTime,firstDateTime, units="mins")+1 
+## minutes in 2 days
+nRowsToRead=24*60*2 
+#read header for col names, 
+DT_0<-fread("data/household_power_consumption.txt", sep = ";", nrows=0) 
+## read data for days; numeric not coerced due to "?" in data
+DT<-fread("data/household_power_consumption.txt", sep = ";",skip=nRowsToSkip, nrows=nRowsToRead ) 
+setnames(DT,names(DT),names(DT_0))
+rm(DT_0)
+
+################ Transform Data if necessary and plot #########################
+# transform data if needed use: DT<-DT[,Voltage:=as.numeric(Voltage)] etc
+# first create on the screen.
+# Create plot array or panel
+par(mfrow=c(2,2))
+# SubPlot1
+with(DT,plot(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),Global_active_power, type = "n", ylab="Global Active Power",xlab=""))
+with(DT,lines(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),Global_active_power, lty=1))
+# SubPlot2
+with(DT,plot(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),Voltage, type = "n", ylab="Voltage",xlab="datetime"))
+with(DT,lines(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),Voltage, lty=1))
+# SubPlot3
+cols=c("black","red","blue")
+with(DT,plot(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),DT[[7]], type = "n", ylab="Energy sub metering",xlab=""))
+for (i in 1:3){
+  # lines for cols: 7 through 9, offset = 6, col=cols[i]
+  with(DT,lines(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),DT[[i+6]], lty=1, col=cols[i]))
+}
+legend("topright", lty=1, col=cols, legend=names(DT)[7:9], bty="n")
+# SubPlot4
+with(DT,plot(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),Global_reactive_power, type = "n",xlab="datetime"))
+with(DT,lines(strptime(paste(DT$Date,DT$Time),"%d/%m/%Y %H:%M:%S"),Global_reactive_power, lty=1))
+
+################ Copy plot to PNG device ######################################
+dev.copy(png,"plot4.png",width=480,height=480)
+dev.off()
